5d64add54397f950511d37c7f7d6a18e51e584e1
S11pedroza <spedroza@utpl.edu.ec>
2024-04-30 11:28:34 -0500

    cambios

--- ejemplo03.py
+++ ejemplo03.py
@@ -1,5 +1,5 @@
 nombre=input("Ingrese nombre de la persona:")
-edad =int(input("INgrese la edad de la persona:"))
+edad =int(input("Ingrese la edad de la persona:"))
 sueldo=float(input("Ingrese el sueldo de la persona:"))
 
 mensajeFinal="Nombre:%s\nEdad:%d\nSueldo:%.2f\n"%(nombre,edad,sueldo)
--- ejemplo08.py
+++ ejemplo08.py
@@ -1,3 +1,6 @@
+diccionario={"nombres":"Stefany","apellidos":"Pedroza Rodriguez","edad":20}
+diccionario["sueldo"]=1000.5
 
+print(diccionario)
 
 
